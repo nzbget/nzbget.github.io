58ec3f45b7893982f6a77e8c4c9a4b7b2b6ffd4b
Andrey Prygunkov <hugbug@users.sourceforge.net>
2017-06-09 16:10:38 +0200

    removed unnecessary (old) redirects

--- _includes/redirects.js
+++
@@ -1,8 +0,0 @@
-var redirects =  {
-	'VideoSort': 'https://github.com/nzbget/VideoSort',
-	'History_(ChangeLog)': 'https://github.com/nzbget/nzbget/releases',
-	'history': 'https://github.com/nzbget/nzbget/releases',
-	'forum': 'http://nzbget.net/forum/index.php',
-	'donation': 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7NCAT7FDRSH5C',
-	'Documentation': 'documentation'
-};
